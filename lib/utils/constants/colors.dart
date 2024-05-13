1bbf585b434cae3ea5fa2d8b2e0cd80fe5ca99e0
Dharma <dharmatejaneralla287@gmail.com>
2024-05-14 00:13:53 +0530

    constants

--- lib/utils/constants/colors.dart
+++ lib/utils/constants/colors.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class Custom_Colors {
-  Custom_Colors._();
+class CustomColors {
+  // App theme colors
   static const Color primary = Color(0xFF4b68ff);
   static const Color secondary = Color(0xFFFFE24B);
   static const Color accent = Color(0xFFb0c7ff);
@@ -18,7 +18,7 @@ class Custom_Colors {
 
   // Background Container colors
   static const Color lightContainer = Color(0xFFF6F6F6);
-  static Color darkContainer = Colors.white.withOpacity(0.1);
+  static Color darkContainer = CustomColors.white.withOpacity(0.1);
 
   // Button colors
   static const Color buttonPrimary = Color(0xFF4b68ff);
